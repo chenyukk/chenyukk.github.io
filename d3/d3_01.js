5ec973ea6be8059341ddfa5ca920a66f944a4d61
scoreace <478872763@qq.com>
2016-11-14 14:23:19 +0800

    11.14.1

---
+++ d3/d3_01.js
@@ -0,0 +1,12 @@
+var output = d3.select('#op');
+        var cir = output.append('circle');
+        cir.attr('cx', 150).attr('cy', 150).attr('r', 50).attr('stroke', 'black').attr('fill','pink').attr('storke-width', '2');
+    
+    
+    var $cir = $("circle");
+    $cir.bind('click',function(){
+    var x = Math.floor(Math.random()*200)+50;
+    var y = Math.floor(Math.random()*200)+50;
+    $cir.attr('cx', x).attr('cy',y);
+
+})
